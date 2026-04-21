1899b09071bc02b3ab1c0fe910d3d3834eaa25bd
Amalkumar G <102892250+AmalkumarG@users.noreply.github.com>
2026-04-21 09:43:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ class TodoHandler(BaseHTTPRequestHandler):
         </head>
         <body>
             <div class="container">
-                <h2>📝 To-Do App22</h2>
+                <h2>📝 To-Do App</h2>
 
                 <form method="POST">
                     <input type="text" name="task" placeholder="Enter task" required>
